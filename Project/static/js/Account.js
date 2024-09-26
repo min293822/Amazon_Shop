3b7f3dbf8de3dcf2650885eacec6a83c03f6feb4
Min Khant <minkhant2t93822@gmail.com>
2024-09-26 17:41:36 +0630

    account

--- Project/static/js/Account.js
+++ Project/static/js/Account.js
@@ -1,20 +1,37 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  const account = document.getElementById("account");
+document.addEventListener("DOMContentLoaded", () => {
   
+  const account = document.getElementById("account");
+  const popupSignUp = document.getElementById("popup-signup");
   const popupLogin = document.getElementById("popup-login");
   const loginBtn = document.getElementById("loginbtn");
   
-  loginbtn.addEventListener("click", ()=>{
+  loginBtn.addEventListener("click", () => {
     account.style.display = "none";
     popupSignUp.style.display = "none";
-   popupLogin.style.display = "flex";
+    popupLogin.style.display = "flex";
   });
   
-  const popupSignUp = document.getElementById("popup-signup");
-  const signUpBtn = document.getElementById("signupbtn");
+  const login = document.getElementById("login");
+  
+  login.addEventListener("click", () => {
+    account.style.display = "block";
+    popupSignUp.style.display = "none";
+    popupLogin.style.display = "none";
+  });
+  
+  const noAccount = document.getElementById("noAccount");
   const name = document.getElementById("full-name");
   
-  signUpBtn.addEventListener("click", ()=>{
+  noAccount.addEventListener("click", () => {
+    account.style.display = "none";
+    popupLogin.style.display = "none";
+    popupSignUp.style.display = "flex";
+    name.style.display = "flex";
+  });
+  
+  const signUpBtn = document.getElementById("signupbtn");
+
+  signUpBtn.addEventListener("click", () => {
     account.style.display = "none";
     popupLogin.style.display = "none";
     popupSignUp.style.display = "flex";
@@ -32,36 +49,56 @@ document.addEventListener("DOMContentLoaded", ()=>{
   const email = document.getElementById("email");
   const password = document.getElementById("password");
 
-nameBtn.addEventListener("click", ()=>{
+  nameBtn.addEventListener("click", () => {
     popupSignUp.style.display = "flex";
     name.style.display = "none";
     birthDate.style.display = "flex";
   });
 
-birthBtn.addEventListener("click", ()=>{
+  birthBtn.addEventListener("click", () => {
     popupSignUp.style.display = "flex";
     birthDate.style.display = "none";
     sex.style.display = "flex";
   });
 
-sexBtn.addEventListener("click", ()=>{
+  sexBtn.addEventListener("click", () => {
     popupSignUp.style.display = "flex";
     sex.style.display = "none";
     email.style.display = "flex";
   });
 
-emailBtn.addEventListener("click", ()=>{
+  emailBtn.addEventListener("click", () => {
     popupSignUp.style.display = "flex";
     email.style.display = "none";
     password.style.display = "flex";
   });
   
-passBtn.addEventListener("click", ()=>{
+  passBtn.addEventListener("click", () => {
     popupSignUp.style.display = "none";
-    account.style.display = "none";
+    account.style.display = "block";
     password.style.display = "none";
   });  
 
+  const showPassword = document.getElementById("showPassw");
+  const passwordInput = document.getElementById("password-input");
+  
+  showPassword.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      passwordInput.setAttribute("type", "text");
+    } else {
+      passwordInput.setAttribute("type", "password");
+    }
+  });
+  
+  const showPass = document.getElementById("show");
+  const passwordLogin = document.getElementById("password-login");
 
-
+  showPass.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      passwordLogin.setAttribute("type", "text");
+    } else {
+      passwordLogin.setAttribute("type", "password");
+    }
+  });
+  
 });
