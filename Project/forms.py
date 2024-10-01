675579f73565b8786715a8215d32de778499609d
Min Khant <minkhant2t93822@gmail.com>
2024-10-01 18:20:03 +0630

    commit

--- Project/forms.py
+++ Project/forms.py
@@ -1,15 +1,26 @@
 from django import forms
-from .models import UserInfo
 from django.contrib.auth.models import User
+from .models import UserInfo
 
 class UserInfoForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    name = forms.CharField(max_length=100)
-    birth_date = forms.DateField(widget=forms.SelectDateWidget(years=range(1950,2025)))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'id':'password'})
+    )
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Full Name', 'id':'name'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email Address', 'id':'email'})
+    )
+    birth_date = forms.DateField(
+        widget=forms.SelectDateWidget(attrs={'class': 'form-control', 'id':'birth'},years=range(1950, 2025))
+    )
+
     class Meta:
         model = UserInfo
-        fields = ['name', 'email', 'password', 'birth_date', 'sex', 'address']
-    
+        fields = ['name', 'email', 'password', 'birth_date']
+
     def save(self, commit=True):
         user = User.objects.create_user(
             username=self.cleaned_data['name'],
