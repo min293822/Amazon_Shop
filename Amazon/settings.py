e4797b6e6a92bd6a176bd9e176bc4b09757a840c
min293822 <minkhant2t93822@gmail.com>
2024-10-24 10:10:53 +0630

    Fix WSGI application path

--- Amazon/settings.py
+++ Amazon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@b8(f40rtwhokxa14toy1piq$k_##2@7ad#985+xy5kny22bl)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
